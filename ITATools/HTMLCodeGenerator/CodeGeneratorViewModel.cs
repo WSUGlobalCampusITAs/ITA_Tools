f24d9db16190c769c0d8ee1b1411c0aaead29bbf
davidlystad <david.lystad@email.wsu.edu>
2015-03-30 11:11:19 -0700

    reverting to previous

---
+++ ITATools/HTMLCodeGenerator/CodeGeneratorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMObjectLibrary;
+
+namespace HTMLCodeGenerator
+{
+    public class CodeGeneratorViewModel : ViewModelBase
+    {
+
+    }
+}
