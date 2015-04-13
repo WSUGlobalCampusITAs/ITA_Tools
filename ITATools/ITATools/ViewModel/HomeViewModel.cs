a8e559448d34cb678bf3c91bb6dc1e624888086a
davidlystad <david.lystad@email.wsu.edu>
2015-04-13 11:58:53 -0700

    Modified Home screen

--- ITATools/ITATools/ViewModel/HomeViewModel.cs
+++ ITATools/ITATools/ViewModel/HomeViewModel.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MVVMObjectLibrary;
+using ITATools.Properties;
 
 namespace ITATools.ViewModel
 {
     public class HomeViewModel : ViewModelBase
     {
+        private string _homeMessage;
 
+        public HomeViewModel()
+        {
+            _homeMessage = Settings.Default.HomeMessage;
+        }
+
+        public string WelcomeMessage { get { return _homeMessage; } }
     }
 }
